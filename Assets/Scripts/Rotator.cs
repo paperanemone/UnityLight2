ac8bd93b3263fdf124430cd9f175aa8c5b524474
paperanemone <unityvs19@gmail.com>
2024-04-19 16:09:50 +0300

    Name Change

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationAroundY : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
--- Assets/Scripts/Scaler.cs
+++ Assets/Scripts/Scaler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SizeIncrease : MonoBehaviour
+public class Scaler : MonoBehaviour
 {
     [SerializeField] private float _speed;
    
