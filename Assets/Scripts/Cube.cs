5fa0843a0d0e6185ac57f00c12ead31b56fed526
paperanemone <unityvs19@gmail.com>
2024-09-12 16:06:16 +0300

    Cubes explosion 2.0

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
     public int SeparationChanceNumber { get; private set; } = 1;
+    public float ExplosionForce { get; private set; } = 100f;
+    public float ExplosionRadius { get; private set; } = 5f;
 
-    public void IncreaseSeparationChanceNumber(GameObject parentCube)
+    public void IncreaseValues(GameObject parentCube)
+    {
+        IncreaseSeparationChanceNumber(parentCube);
+        IncreaseExplosionForce(parentCube);
+        IncreaseExplosionRadius(parentCube);
+    }
+
+    private void IncreaseSeparationChanceNumber(GameObject parentCube)
     {
         SeparationChanceNumber = parentCube.GetComponent<Cube>().SeparationChanceNumber * 2;
+    }
+
+    private void IncreaseExplosionForce(GameObject parentCube)
+    {
+        ExplosionForce = parentCube.GetComponent<Cube>().ExplosionForce * 2;
+    }
+
+    private void IncreaseExplosionRadius(GameObject parentCube)
+    {
+        ExplosionRadius = parentCube.GetComponent<Cube>().ExplosionRadius * 2;
     }
 }
--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -1,8 +1,9 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CubeManager : MonoBehaviour
 {
+    [SerializeField] private LayerMask _layerMask;
+
     private int _minRandomCubeCount = 2;
     private int _maxRandomCubeCount = 6;
 
@@ -11,24 +12,33 @@ public class CubeManager : MonoBehaviour
 
     public void ExplodeCube(GameObject cube)
     {
-            if (Random.Range(1, cube.GetComponent<Cube>().SeparationChanceNumber + 1) == 1)
+        if (Random.Range(1, cube.GetComponent<Cube>().SeparationChanceNumber + 1) == 1)
+        {
+            int randomCubeCount = Random.Range(_minRandomCubeCount, _maxRandomCubeCount + 1);
+
+            for (int i = 0; i < randomCubeCount; i++)
             {
-                int randomCubeCount = Random.Range(_minRandomCubeCount, _maxRandomCubeCount + 1);
-
-                for (int i = 0; i < randomCubeCount; i++)
-                {
-                    CreateNewCube(cube).GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, cube.transform.position, _explosionRadius);
-                }
+                CreateNewCube(cube).GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, cube.transform.position, _explosionRadius);
             }
+        }
+        else
+        {
+            Collider[] colliders = Physics.OverlapSphere(cube.transform.position, _explosionRadius, _layerMask.value);
+                
+           foreach (Collider collider in colliders)
+           {
+               collider.attachedRigidbody.AddExplosionForce(cube.GetComponent<Cube>().ExplosionForce, cube.transform.position, cube.GetComponent<Cube>().ExplosionRadius);
+           }
+        }
 
-            Destroy(cube);
+        Destroy(cube);
     }
 
     private GameObject CreateNewCube(GameObject cube)
     {
         GameObject newCube = Instantiate(cube);
 
-        newCube.GetComponent<Cube>().IncreaseSeparationChanceNumber(cube);
+        newCube.GetComponent<Cube>().IncreaseValues(cube);
 
         newCube.transform.localScale = cube.transform.localScale / 2;
 
